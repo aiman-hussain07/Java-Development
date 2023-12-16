b7ed52f111a548ce9bd394dbe684b1e827d656dd
Aiman Hussain <aimanhussain2698@gmail.com>
2023-12-16 13:20:24 +0530

    Variables and Data Types

---
+++ PartB/JavaBasics.java
@@ -0,0 +1,16 @@
+package PartB;
+
+import java.util.Scanner;
+// import java.lang.Math;
+
+public class JavaBasics {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        float pencil = sc.nextFloat();
+        float pen = sc.nextFloat();
+        float eraser = sc.nextFloat();
+        float bill = ((pencil + pen + eraser) * 118) / 100;
+        System.out.println(bill);
+
+    }
+}
