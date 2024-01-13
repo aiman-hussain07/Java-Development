91a716d8d69e60d1c78c9f48d559170ceabdeac0
Aiman Hussain <aimanhussain2698@gmail.com>
2024-01-13 19:41:43 +0530

    PatternII

--- PartF_Patterns1/pattern.java
+++ PartF_Patterns1/pattern.java
@@ -30,9 +30,9 @@ class pattern {
 
         // Half Pyramid Pattern
         // for (int i = 1; i <= n; i++) {
-        // for (int j = 1; j <= i; j++) {
-        // System.out.print(j);
-        // }
+            // for (int j = 1; j <= i; j++) {
+            // System.out.print(j);
+            // }
         // System.out.println();
         // }
 
