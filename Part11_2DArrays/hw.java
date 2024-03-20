5d45a4f00ef498c9dd0a6b0f3644f0be8fcff032
Aiman Hussain <aimanhussain2698@gmail.com>
2024-03-20 18:55:00 +0530

    Strings

--- Part11_2DArrays/hw.java
+++ Part11_2DArrays/hw.java
@@ -36,8 +36,8 @@ class hw {
         int row = matrix.length, col = matrix[0].length;
         // Create New Matrix
         int transpose[][] = new int[col][row];
-        for (int i = 0; i < col; i++) {
-            for (int j = 0; j < row; j++) {
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
                 transpose[j][i] = matrix[i][j];
             }
         }
